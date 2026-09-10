233fdd95de64bab7a439c5f9e214fb7842cbedad
parsamrrelax <parsa.osunlou1998@gmail.com>
2026-09-10 20:04:53 +0330

    Bump version to 1.1.0

--- lib/backup/backup_metadata.dart
+++ lib/backup/backup_metadata.dart
@@ -6,7 +6,7 @@ class BackupMetadata {
     required this.createdAt,
     required this.stickerCount,
     required this.boardCount,
-    this.appVersion = '1.0.0',
+    this.appVersion = '1.1.0',
   });
 
   final int version;
